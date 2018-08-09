2a94373154ca83f5f6a2d56cf562b20731a2e957
Henocks <hr4574@naver.com>
2018-08-09 09:05:22 +0900

    Descripto Remove

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './registerServiceWorker';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,81 +1,79 @@
 import * as types from '../actions/ActionTypes';
 
-// 초기 상태를 정의합니다.
 const initialState = {
-	counter: 0,
-	todoCards: [
-		{
-			id: 0,
-			isFinished: 0,
-			todoContent: "CARD_INIT"
-		}
-	]
+  counter: 0,
+  todoCards: [
+    {
+      id: 0,
+      isFinished: 0,
+      todoContent: "CARD_INIT"
+    }
+  ]
 }
 
 function todoCard(state = initialState, action) {
-	const { todoCards } = state;
+  const { todoCards } = state;
 
-	console.log("reducer worked!");
-	console.log(action);
-	console.log(action.type);
+  console.log("reducer worked!");
+  console.log(action);
+  console.log(action.type);
 
-	switch (action.type) {
-		case types.CREATE:
-			return {
-				counter: state.counter + 1,
-				todoCards: [
-					...todoCards,
-					{
-						id: state.counter + 1,
-						isFinished: 0,
-						todoContent: action.content
-					}
-				]
-			};
+  switch (action.type) {
+    case types.CREATE:
+      return {
+        counter: state.counter + 1,
+        todoCards: [
+          ...todoCards,
+          {
+            id: state.counter + 1,
+            isFinished: 0,
+            todoContent: action.content
+          }
+        ]
+      };
 
-		case types.REMOVE:
-			console.log('remove : ' + action.index);
-			return {
-				counter: state.counter,
-				todoCards: [
-					...todoCards.filter(todoCards => todoCards.id !== action.index)
-				]
-			};
+    case types.REMOVE:
+      console.log('remove : ' + action.index);
+      return {
+        counter: state.counter,
+        todoCards: [
+          ...todoCards.filter(todoCards => todoCards.id !== action.index)
+        ]
+      };
 
 
-		case types.UPDATE:
-			console.log('update : ' + action.index);
-			return {
-				counter: state.counter,
-				todoCards: [
-					...todoCards.slice(0, action.index),
-					{
-						...todoCards[action.index],
-						todoContent: action.content
-					},
-					...todoCards.slice(action.index + 1, todoCards.length)
-				]
-			};
+    case types.UPDATE:
+      console.log('update : ' + action.index);
+      return {
+        counter: state.counter,
+        todoCards: [
+          ...todoCards.slice(0, action.index),
+          {
+            ...todoCards[action.index],
+            todoContent: action.content
+          },
+          ...todoCards.slice(action.index + 1, todoCards.length)
+        ]
+      };
 
-		case types.COMPLETE:
-			console.log('complete : ' + action.index);
-			return {
-				counter: state.counter,	
-				todoCards: [
-					...todoCards.slice(0, action.index),
-					{
-						...todoCards[action.index],
-						isFinished: 1
-					},
-					...todoCards.slice(action.index + 1, todoCards.length)
-				]
-			};
+    case types.COMPLETE:
+      console.log('complete : ' + action.index);
+      return {
+        counter: state.counter,
+        todoCards: [
+          ...todoCards.slice(0, action.index),
+          {
+            ...todoCards[action.index],
+            isFinished: 1
+          },
+          ...todoCards.slice(action.index + 1, todoCards.length)
+        ]
+      };
 
-		default:
-			console.log("DEFAULT");
-			return state;
-
-	}
+    default:
+      console.log("DEFAULT");
+      return state;
+  }
 }
 
 export default todoCard;
